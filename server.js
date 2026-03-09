4fb6161dc091c0e8beb905b3c2ed296c25182733
Bhakti Takey <bhakti@Bhaktis-MacBook-Air.local>
2026-03-09 14:40:54 +0530

    Change sender name to BVRITH

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ app.post('/api/student/request', authenticate, async (req, res) => {
         console.log('To:', parentEmail);
         
         await resend.emails.send({
-            from: `BVRIT Admin <${process.env.EMAIL_USER}>`,
+            from: `BVRITH <${process.env.EMAIL_USER}>`,
             to: parentEmail,
             subject: `Leave Request from ${user.name}`,
             html: `
